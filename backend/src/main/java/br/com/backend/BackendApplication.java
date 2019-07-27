a77626d9fb34d6c412bb617a4598bdc53170a8f5
RODRIGO-PC\Rodrigo <rodrigo.soares@bookhair.com.br>
2019-07-26 22:40:59 -0300

    Criação de relacionamento manyToMany

--- backend/src/main/java/br/com/backend/BackendApplication.java
+++ backend/src/main/java/br/com/backend/BackendApplication.java
@@ -9,7 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import br.com.backend.domain.Categoria;
+import br.com.backend.domain.Produto;
 import br.com.backend.repositories.CategoriaRepository;
+import br.com.backend.repositories.ProdutoRepository;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -22,6 +24,9 @@ public class BackendApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	@Bean
     public Docket swagger() {
@@ -43,7 +48,19 @@ public class BackendApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informárica");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto produto1 = new Produto(null, "Computador", 2000.00);
+		Produto produto2 = new Produto(null, "Impressora", 2000.00);
+		Produto produto3 = new Produto(null, "Mouse", 2000.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
+		cat2.getProdutos().add(produto2);
+		
+		produto1.getCategorias().add(cat1);
+		produto2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		produto3.getCategorias().add(cat1);
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
 		
 		
 		
